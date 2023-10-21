dcda3122c9dde93f2467afb4bc03179e8d96f4c7
Nyssaix <brandoncaron@hotmail.ca>
2023-10-20 23:46:05 -0600

    Two simple alghorithms to count vowels and consonants for a hardcoded string

--- u5_lab2.py
+++ u5_lab2.py
@@ -1,1 +1,20 @@
-# 
+# Basic definition of vowels and consonants as either a str or list value, subject to change 
+
+vowels = "aeiouAEIOU"
+consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+name = "John Doe"
+vowel_count = 0
+consonant_count = 0
+
+
+for letter in name:
+    if letter in vowels:
+        vowel_count += 1
+
+for letter in name:
+    if letter in consonants:
+        consonant_count += 1
+
+
+print(vowel_count)
+print(consonant_count)
